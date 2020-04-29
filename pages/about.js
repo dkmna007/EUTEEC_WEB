95fe9425f1c38eb096a16f2a91660c284d30385f
Eric Wahome <eric.wahome@tingle.software>
2020-04-29 20:31:06 +0300

    updated

--- pages/about.js
+++ pages/about.js
@@ -6,6 +6,11 @@ import { Container, Header } from "@/components";
 import { Constants } from "@/constants/About";
 import Head from "next/head";
 
+/* components */
+import LocationCard from "@/components/Cards/LocationCard";
+import ActivityCard from "@/components/Cards/ActivityCard";
+import MembershipCard from "@/components/Cards/MembershipCard";
+
 const useStyles = makeStyles(theme => ({
   root: {
     // margin: theme.spacing(0, 2)
@@ -79,7 +84,91 @@ const About = () => {
         Current={staticAbout.Current}
         image={"/assets/images/tech1.jpg"}
       />
-      <Container background={"#17141d"}>About Page</Container>
+      <Container background={"#17141d"}>
+        <br />
+        {/* MEMBERSHIP*/}
+        <Grid container justify="center" spacing={1}>
+          <Grid item sm={6} md={6} xs={12}>
+            <MembershipCard membership={membership} />
+          </Grid>
+
+          {/*LOCATION SECTION */}
+          <Grid item sm={6} md={6} xs={12}>
+            <LocationCard title={"LOCATION"} />
+            <Box>
+              <img className={classes.logo} src={appLogo} title="euteec" />
+              <Typography variant="inherit">{euteecDescription}</Typography>
+            </Box>
+          </Grid>
+        </Grid>
+      </Container>
+
+      <br />
+      <br />
+
+      <Divider />
+      <Container
+        elevation={0}
+        top={2}
+        sides={"0px"}
+        background={"rgba(255, 255, 255, 0)"}
+      >
+        {/* TEAM SECTION*/}
+        {/* END OF TEAM */}
+        <br />
+        <Divider />
+        <br />
+
+        {/* ACTIVITIES SECTION*/}
+
+        <Grid container justify="space-evenly" spacing={2}>
+          <Grid item xs={12}>
+            <Typography
+              color="primary"
+              align="center"
+              variant="h6"
+              className={classes.subheading}
+            >
+              ACTIVITIES
+            </Typography>
+            <div className={classes.underline}></div>
+            <br />
+          </Grid>
+          {activities.map(activity => {
+            return (
+              <Grid item md={6} sm={12} xs={12}>
+                <ActivityCard title={"ACTIVITIES"} activity={activity} />
+              </Grid>
+            );
+          })}
+        </Grid>
+
+        <br />
+        <br />
+        {/* PARTNERS SECTION */}
+
+        <Grid container justify="center" spacing={1}>
+          <Grid item xs={12}>
+            <Typography
+              color="primary"
+              align="center"
+              variant="h6"
+              className={classes.subheading}
+            >
+              OUR PARTNERS
+            </Typography>
+          </Grid>
+          <br />
+          <Divider />
+          {partners.map(partner => {
+            return (
+              <Grid item={6}>
+                <Typography variant="inherit">{partner.PartnerName}</Typography>
+              </Grid>
+            );
+          })}
+        </Grid>
+      </Container>
     </div>
   );
 };
