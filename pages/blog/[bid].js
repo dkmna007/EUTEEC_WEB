3891346f51f478b01a891742e724f55ba3aa90a4
Eric Wahome <eric.wahome@tingle.software>
2020-04-30 14:04:47 +0300

    updated

--- pages/blog/[bid].js
+++ pages/blog/[bid].js
@@ -48,32 +48,26 @@ export default function Blog({ blog }) {
     <DefaultLayout menuProps={menuProps}>
       <div className={classes.root}>
         <Head>
-          <title>Euteec Blog</title>
+          <title>Blog</title>
           <link rel="icon" href="/favicon.ico" />
           <meta
             property="og:url"
-            content={
-              "http://www.nytimes.com/2015/02/19/arts/international/when-great-minds-dont-think-alike.html"
-            }
+            content="http://www.nytimes.com/2015/02/19/arts/international/when-great-minds-dont-think-alike.html"
             key="url"
           />
-          <meta property="og:type" content="article" key="article" />
-          <meta property="og:title" content={blog.title} key="title" />
+          <meta property="og:type" content="article" key="type" />
+          <meta property="og:title" content="Think Loud" key="title" />
           <meta
             property="og:description"
-            content={blog.content}
+            content="creative thinking?"
             key="description"
           />
-          <meta property="og:image" content={blog.mediaUrl} key="image" />
+          <meta
+            property="og:image"
+            content="http://res.cloudinary.com/dxcci3wkx/image/upload/v1585837838/iapc5e6rsubnherxv17q.jpg"
+            key="image"
+          />
         </Head>
-        <Header
-          title={blog && blog.title}
-          subTitle={
-            "A Place for Tech Enthuasists to Share and Discover New Stories"
-          }
-          // Current={staticAbout.Current}
-          image={"https://source.unsplash.com/user/erondu/1600x900"}
-        />
         <Container background={"#17141d"}>
           {blog && (
             <Grid container justify="space-evenly" spacing={1}>
